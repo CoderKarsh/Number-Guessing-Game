8f1e34b84a93431797647109ca486293425a010e
CoderKarsh <98488718+CoderKarsh@users.noreply.github.com>
2024-01-19 20:42:01 +0530

    Update script.js
    Centering the Reset button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecting a number to be guessed
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 // Select the paragraphs for displaying results.
@@ -8,7 +9,7 @@ const lowOrHi = document.querySelector(".lowOrHi");
 // Selecting the inputs - number and submit
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
-
+guessField.textContent = "";
 // Initializing guessCount to 1
 let guessCount = 1;
 let resetButton;
@@ -19,7 +20,6 @@ guessField.focus();
 function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
-        
       guesses.textContent = "Previous guesses: ";
     }
     guesses.textContent += `${userGuess} `;
@@ -27,7 +27,7 @@ function checkGuess() {
     if (userGuess === randomNumber) {
       lastResult.textContent = "Congratulations! You got it right!";
       lastResult.style.backgroundColor = "rgba(119,221,119)";
-      lastResult.style.border = "2px solid green";
+      lastResult.style.border = "3px solid green";
       lastResult.style.borderRadius = "0.25rem";
       lastResult.style.fontWeight = "600";
       lastResult.style.color = "green";
@@ -42,7 +42,7 @@ function checkGuess() {
     } else {
       lastResult.textContent = "Wrong!";
       lastResult.style.backgroundColor = "rgba(250, 160, 160)";
-      lastResult.style.border = "2px solid red";
+      lastResult.style.border = "3px solid red";
       lastResult.style.borderRadius = "0.25rem";
       lastResult.style.color = "red";
       lastResult.style.fontWeight = "600";
@@ -65,7 +65,7 @@ function setGameOver() {
     guessField.disabled = true;
     guessSubmit.disabled = true;
     resetButton = document.createElement("button");
-    resetButton.style.border = "2px solid black";
+    resetButton.style.border = "3px solid black";
     resetButton.style.borderRadius = "4px";
     resetButton.style.padding = "0.5rem 1rem";
     resetButton.style.fontWeight = "600";
@@ -73,6 +73,9 @@ function setGameOver() {
     resetButton.style.backgroundColor = "#f3aa60"
     resetButton.style.position = "absolute";
     resetButton.style.bottom = "1rem";
+    resetButton.style.left = "50%";
+    resetButton.style.transform = "translateX(-50%)";
+    resetButton.style.cursor = "pointer";
     
     resetButton.textContent = "Start New Game";
     
@@ -83,6 +86,5 @@ function setGameOver() {
 
 function resetGame() {
     location.reload();
+    guessField.textContent = ""; 
   }
-  
-  
